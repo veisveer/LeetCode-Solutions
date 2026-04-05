8a4a690ac50a4f77bf0269742a878c418b4befcf
Renee Veisveer <renee@veisveer.ee>
2026-04-05 19:38:05 +0300

    Time: 6 ms (64.13%) | Memory: 56.7 MB (78.26%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.ts
@@ -0,0 +1,17 @@
+function judgeCircle(moves: string): boolean {
+    let x = 0, y = 0;
+
+    for (const move of moves) {
+        if (move === 'R') {
+            x++;
+        } else if (move === 'L') {
+            x--;
+        } else if (move === 'U') {
+            y++;
+        } else if (move === 'D') {
+            y--;
+        }
+    }
+
+    return (x === 0 && y === 0);
+};
